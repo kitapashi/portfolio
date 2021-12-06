2328af7b799ccc56029a2335db3ad7085c9d9459
YukiKitahashi <pash76573@gmail.com>
2021-12-06 07:42:55 +0000

    create footer

---
+++ my-next-site/components/Footer.tsx
@@ -0,0 +1,75 @@
+import {
+  Box,
+  chakra,
+  Container,
+  Stack,
+  Text,
+  useColorModeValue,
+  VisuallyHidden,
+} from '@chakra-ui/react';
+import { FaInstagram, FaGithub, FaTwitter } from 'react-icons/fa';
+import { ReactNode } from 'react';
+
+const SocialButton = ({
+  children,
+  label,
+  href,
+}: {
+  children: ReactNode;
+  label: string;
+  href: string;
+}) => {
+  return (
+    <chakra.button
+      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+      rounded={'full'}
+      w={8}
+      h={8}
+      cursor={'pointer'}
+      as={'a'}
+      href={href}
+      display={'inline-flex'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      transition={'background 0.3s ease'}
+      _hover={{
+        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
+      }}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {children}
+    </chakra.button>
+  );
+};
+
+export default function SmallWithSocial() {
+  const date = new Date();
+
+  return (
+    <Box
+      bg={useColorModeValue('gray.50', 'gray.900')}
+      color={useColorModeValue('gray.700', 'gray.200')}>
+      <Container
+        as={Stack}
+        maxW={'8xl'}
+        py={4}
+        direction={{ base: 'column', md: 'row' }}
+        spacing={4}
+        justify={{ base: 'center', md: 'space-between' }}
+        align={{ base: 'center', md: 'center' }}
+      >
+        <Text>© {date.getFullYear()} Yuki Kitahashi. All rights reserved</Text>
+        <Stack direction={'row'} spacing={6}>
+          <SocialButton label={'Twitter'} href={'https://twitter.com/folky_records'}>
+            <FaTwitter />
+          </SocialButton>
+          <SocialButton label={'GitHub'} href={'https://github.com/kitapashi'}>
+            <FaGithub />
+          </SocialButton>
+          <SocialButton label={'Instagram'} href={'https://www.instagram.com/kitapashi'}>
+            <FaInstagram />
+          </SocialButton>
+        </Stack>
+      </Container>
+    </Box>
+  );
+}
