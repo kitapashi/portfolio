69aec1d9f5f29578ea952c145266c72ee8530d89
YukiKitahashi <pash76573@gmail.com>
2021-12-10 12:40:58 +0000

    fix WorksCard

--- my-next-site/components/molecules/WorksCard.tsx
+++ my-next-site/components/molecules/WorksCard.tsx
@@ -1,4 +1,4 @@
-import { Badge, Box, VStack, Heading, Text, Image, HStack } from '@chakra-ui/react';
+import { Link, Box, VStack, Heading, Text, Image, HStack } from '@chakra-ui/react';
 import WorkBadge, { WorkBadgeType } from '../atomic/WorkBadges';
 
 
@@ -7,34 +7,48 @@ export interface WorksCardProps {
   description: string;
   type: WorkBadgeType;
   imageSrc: string;
+  path: string
 }
 
 const WorksCard: React.FC<WorksCardProps> = (props: WorksCardProps) => {
   return (
-    <Box
-      position="relative"
-      bg="gray.200"
-      boxShadow="xl"
-      rounded="xl"
-      maxH="500px"
-      maxW="400px"
-      p={3}
+    <Link
+    px={2}
+    py={1}
+    rounded={'md'}
+    href={props.path}
+    isExternal
+    maxW="400px"
+    _hover={{
+      textDecoration: 'none',
+      bg: 'gray.200',
+    }}
     >
-      <VStack spacing="2" w="100%">
-        <Image src={props.imageSrc} 
-          fit="cover" 
-          h="270px"
-          w="100%"
-        />
-        <Box w="100%" textAlign="center">
-          <Heading as="h3" size="md" isTruncated py={3}>{props.title}</Heading>
-          <HStack>
-            <WorkBadge type={props.type} />
-            <Text as="p" isTruncated color="gray.500">{props.description}</Text>
-          </HStack>
-        </Box>
-      </VStack>
-    </Box>
+      <Box
+        position="relative"
+        bg="gray.200"
+        boxShadow="xl"
+        rounded="xl"
+        maxH="500px"
+        maxW="100%"
+        p={3}
+        >
+        <VStack spacing="2" w="100%">
+          <Image src={props.imageSrc} 
+            fit="cover" 
+            h="270px"
+            w="100%"
+            />
+          <Box w="100%" textAlign="center">
+            <Heading as="h3" size="md" isTruncated py={3}>{props.title}</Heading>
+            <HStack>
+              <WorkBadge type={props.type} />
+              <Text as="p" isTruncated color="gray.500">{props.description}</Text>
+            </HStack>
+          </Box>
+        </VStack>
+      </Box>
+    </Link>
   );
 };
 
--- my-next-site/contents/works/worksData.tsx
+++ my-next-site/contents/works/worksData.tsx
@@ -1,42 +1,29 @@
 import { WorksCardProps } from "../../components/molecules/WorksCard"
-import pic from '../../public/top.png';
 import slsqiita from '../../public/slsqiita.jpeg';
 import cfqiita from '../../public/cfqiita.jpeg';
 import best2020 from '../../public/2020best.jpeg';
 
-interface WorksData extends WorksCardProps {
-  link: string
-}
-
-
-const data: WorksData[] = [
+const data: WorksCardProps[] = [
   {
     title: "私的2020年ベストアルバム",
     description: "[note]2020年ベスト新譜のレビュー記事",
     imageSrc: best2020.src,
     type: "music",
-    link: "https://note.com/kitapashi/n/nd2e19eb9feac" 
+    path: "https://note.com/kitapashi/n/nd2e19eb9feac" 
   },
   {
     title: "[Serverless Framework]はじめの一歩の次のポイントを紹介する",
     description: "[Qiita] Serverless Advent Calendar 2020",
     imageSrc: slsqiita.src,
     type: "tech",
-    link: "https://qiita.com/folky_records/items/15869693c7d6ec9b1716" 
+    path: "https://qiita.com/folky_records/items/15869693c7d6ec9b1716" 
   },
   {
     title: "CloudFront+API Gateway構成ではHostヘッダに気をつけよう",
     description: "[Qiita]",
     imageSrc: cfqiita.src,
     type: "tech",
-    link: "https://qiita.com/folky_records/items/2a231b14c08be82a183a" 
-  },
-  {
-    title: "4",
-    description: "fuga",
-    imageSrc: pic.src,
-    type: "other",
-    link: "https://google.com" 
+    path: "https://qiita.com/folky_records/items/2a231b14c08be82a183a" 
   },
 ]
 
